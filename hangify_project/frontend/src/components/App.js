bb553f0bd28f1136be9d2d2f98af49dae6569f6d
pabsanort2 <pabsanort2@alum.us.es>
2022-06-22 15:16:06 +0200

    Episode #4: Routing with React
    Made some React components and routing for the joining and creating rooms pages.

--- hangify_project/frontend/src/components/App.js
+++ hangify_project/frontend/src/components/App.js
@@ -1,15 +1,22 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return(<h1>Testing React Code</h1>);
-    }
+  render() {
+    return (
+      <div>
+        <HomePage></HomePage>
+      </div>
+    );
+  }
 }
 
-const appDiv = document.getElementById("app")
-render(<App/>, appDiv);
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
