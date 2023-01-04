7d5f8b25c79e667d5654e1f7f401c6e0fb15b328
Pablo <pabsanort2@alum.us.es>
2023-01-04 11:30:56 +0100

    Episode #8: Join Page functinality

--- hangify_project/api/urls.py
+++ hangify_project/api/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 
 urlpatterns = [
     path('home', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
 ]
